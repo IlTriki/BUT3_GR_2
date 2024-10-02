b1fe74b08a737daea908715da373e3460c985b85
Triki Osama <trikiosama@gmail.com>
2024-10-02 11:14:24 +0200

    bd script pour la branche cryptage

--- script/dumpSQL.sql
+++ script/dumpSQL.sql
@@ -1,37 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 02, 2024 at 11:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `allard19u_coa_banque`
+-- Database: `qualdev2024`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Compte`
+-- Table structure for table `compte`
 --
 
-CREATE TABLE IF NOT EXISTS `Compte` (
+CREATE TABLE `compte` (
   `numeroCompte` varchar(50) NOT NULL,
   `userId` varchar(50) NOT NULL,
   `solde` double NOT NULL,
   `avecDecouvert` varchar(5) NOT NULL,
-  `decouvertAutorise` decimal(10,0) DEFAULT NULL,
-  PRIMARY KEY (`numeroCompte`),
-  KEY `index_userClient` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `decouvertAutorise` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `Compte`
+-- Dumping data for table `compte`
 --
 
-INSERT INTO `Compte` (`numeroCompte`, `userId`, `solde`, `avecDecouvert`, `decouvertAutorise`) VALUES
+INSERT INTO `compte` (`numeroCompte`, `userId`, `solde`, `avecDecouvert`, `decouvertAutorise`) VALUES
 ('AB7328887341', 'client2', -97, 'AVEC', 123),
 ('BD4242424242', 'client1', 150, 'SANS', NULL),
 ('FF5050500202', 'client1', 705, 'SANS', NULL),
@@ -47,41 +55,58 @@ INSERT INTO `Compte` (`numeroCompte`, `userId`, `solde`, `avecDecouvert`, `decou
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Utilisateur`
+-- Table structure for table `utilisateur`
 --
 
-CREATE TABLE IF NOT EXISTS `Utilisateur` (
+CREATE TABLE `utilisateur` (
   `userId` varchar(50) NOT NULL,
   `nom` varchar(45) NOT NULL,
   `prenom` varchar(45) NOT NULL,
   `adresse` varchar(100) NOT NULL,
-  `userPwd` varchar(45) DEFAULT NULL,
+  `userPwd` varchar(255) DEFAULT NULL,
   `male` bit(1) NOT NULL,
   `type` varchar(10) NOT NULL,
-  `numClient` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `numClient_UNIQUE` (`numClient`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `numClient` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `Utilisateur`
+-- Dumping data for table `utilisateur`
 --
 
-INSERT INTO `Utilisateur` (`userId`, `nom`, `prenom`, `adresse`, `userPwd`, `male`, `type`, `numClient`) VALUES
-('a', 'a', 'a', 'a', 'a', b'1', 'MANAGER', NULL),
-('admin', 'Smith', 'Joe', '123, grande rue, Metz', 'adminpass', b'1', 'MANAGER', ''),
-('client1', 'client1', 'Jane', '45, grand boulevard, Brest', 'clientpass1', b'1', 'CLIENT', '123456789'),
-('client2', 'client2', 'Jane', '45, grand boulevard, Brest', 'clientpass2', b'1', 'CLIENT', '123456788');
+INSERT INTO `utilisateur` (`userId`, `nom`, `prenom`, `adresse`, `userPwd`, `male`, `type`, `numClient`) VALUES
+('a', 'a', 'a', 'a', '$2y$10$2yBxEHp9EookQ1QCvwkOK.z9ZvVz2hRYZj8RNpUFfWYEqQyb4vurS', b'1', 'MANAGER', NULL),
+('admin', 'Smith', 'Joe', '123, grande rue, Metz', '$2y$10$vkfWxGqEiBAd4bpUNwcw3.BinZ7K3Pk3dykN08mW.24h/Vv87OxcG', b'1', 'MANAGER', ''),
+('client1', 'client1', 'Jane', '45, grand boulevard, Brest', '$2a$10$U53XoilPbiqB9RG0O5/2n.6V10kd0f3lzpwuEzECWmLp1JpDIty/S', b'1', 'CLIENT', '123456789'),
+('client2', 'client2', 'Jane', '45, grand boulevard, Brest', '$2y$10$dFNqrmxsX0prFyjfsHomIOaD2WaGEiKDBX8OVxcAcRmy4ZjEUU/rW', b'1', 'CLIENT', '123456788');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `compte`
+--
+ALTER TABLE `compte`
+  ADD PRIMARY KEY (`numeroCompte`),
+  ADD KEY `index_userClient` (`userId`);
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `numClient_UNIQUE` (`numClient`);
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `Compte`
+-- Constraints for table `compte`
 --
-ALTER TABLE `Compte`
-  ADD CONSTRAINT `fk_Compte_userId` FOREIGN KEY (`userId`) REFERENCES `Utilisateur` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `compte`
+  ADD CONSTRAINT `fk_Compte_userId` FOREIGN KEY (`userId`) REFERENCES `utilisateur` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
