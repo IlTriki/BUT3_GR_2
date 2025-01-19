0fb0b606df0e1e101eccc63d7373857de5efb85e
CHAMPES <nathan.champes@gmail.com>
2025-01-19 23:19:01 +0100

    Modif du test

--- src/test/java/com/iut/banque/test/controller/TestsListeCompteManager.java
+++ src/test/java/com/iut/banque/test/controller/TestsListeCompteManager.java
@@ -2,6 +2,7 @@ package com.iut.banque.test.controller;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.iut.banque.controller.ListeCompteManager;
+import com.iut.banque.exceptions.IllegalFormatException;
 import com.iut.banque.modele.Client;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,9 +25,15 @@ public class TestsListeCompteManager {
 
     @Test
     public void testSetAndGetClient() {
-        Client client = new Client("Test", "User", "Address", true, "t.user1", "password", "1234567890");
-        listeCompteManager.setClient(client);
-        assertNotNull(listeCompteManager.getClient());
-        assertEquals(client, listeCompteManager.getClient());
+        try {
+            Client client = new Client("Test", "User", "Address", true, "t.user1", "password", "1234567890");
+            listeCompteManager.setClient(client);
+            assertNotNull(listeCompteManager.getClient());
+            assertEquals(client, listeCompteManager.getClient());
+        } catch (IllegalFormatException e) {
+            fail("IllegalFormatException récupérée : " + e.getMessage());
+        } catch (Exception e) {
+            fail("Exception inattendue récupérée : " + e.getMessage());
+        }
     }
 } 
